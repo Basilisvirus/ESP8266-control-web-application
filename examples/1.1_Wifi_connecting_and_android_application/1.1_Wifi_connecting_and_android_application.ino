24d8763259e1361a33f453e9159a08f2f4246f43
Basilisvirus <basilisvirus@hotmail.com>
2019-06-03 21:19:32 +0300

    added all files

---
+++ examples/1.1_Wifi_connecting_and_android_application/1.1_Wifi_connecting_and_android_application.ino
@@ -0,0 +1,180 @@
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+const char* ssid = "000000";
+const char* password = "000000";
+
+ESP8266WebServer server(80);
+
+const int output1 = 4;
+const int output2 = 5;
+const int output3 = 12;
+const int output4 = 14;
+
+boolean device1 = false;
+boolean device2 = false;
+boolean device3 = false;
+boolean device4 = false;
+//===================================FUNCTIONS===============================================
+void handleRoot() {
+
+  String cmd;     
+      cmd += "<!DOCTYPE HTML>\r\n";
+      cmd += "<html>\r\n";
+      //cmd += "<header><title>ESP8266 Webserver</title><h1>\"ESP8266 Web Server Control\"</h1></header>";
+      cmd += "<head>";
+      cmd += "<meta http-equiv='refresh' content='100'/>";
+      cmd += "</head>";
+      
+    //<!--Toggle Device1 ON-->
+      cmd+="<p>\r\n <form action=\"/status1=1\" method=\"POST\">\r\n <input type=\"submit\" value=\"Toggle Device1 ON\">\r\n </form>\r\n </p>\r\n";
+    
+    //<!--Toggle Device1 OFF-->
+      cmd+="<p>\r\n <form action=\"/status1=0\" method=\"POST\">\r\n <input type=\"submit\" value=\"Toggle Device1 OFF\">\r\n </form>\r\n </p>\r\n";
+   
+    //<!--Toggle Device2 ON-->
+      cmd+="<p>\r\n <form action=\"/status2=1\" method=\"POST\">\r\n <input type=\"submit\" value=\"Toggle Device2 ON\">\r\n </form>\r\n </p>\r\n";
+
+    //<!--Toggle Device2 OFF-->
+      cmd+="<p>\r\n <form action=\"/status2=0\" method=\"POST\">\r\n <input type=\"submit\" value=\"Toggle Device2 OFF\">\r\n </form>\r\n </p>\r\n";
+
+    //<!--Toggle Device3 ON-->
+      cmd+="<p>\r\n <form action=\"/status3=1\" method=\"POST\">\r\n <input type=\"submit\" value=\"Toggle Device3 ON\">\r\n </form>\r\n </p>\r\n";
+
+    //<!--Toggle Device3 OFF-->
+      cmd+="<p>\r\n <form action=\"/status3=0\" method=\"POST\">\r\n <input type=\"submit\" value=\"Toggle Device3 OFF\">\r\n </form>\r\n </p>\r\n";
+
+    //<!--Toggle Device4 ON-->
+      cmd+="<p>\r\n <form action=\"/status4=1\" method=\"POST\">\r\n <input type=\"submit\" value=\"Toggle Device4 ON\">\r\n </form>\r\n </p>\r\n";
+
+    //<!--Toggle Device4 OFF-->
+      cmd+="<p>\r\n <form action=\"/status4=0\" method=\"POST\">\r\n <input type=\"submit\" value=\"Toggle Device4 OFF\">\r\n </form>\r\n </p>\r\n";
+      
+      if(device1){
+        cmd +=("<br/>Device1  : ON");
+      }
+      else{
+        cmd +=("<br/>Device1  : OFF");
+      }
+      
+      if(device2){
+        cmd +=("<br/>Device2  : ON");
+      }
+      else{
+        cmd +=("<br/>Device2  : OFF");
+      }
+           
+       if(device3){
+        cmd +=("<br/>Device3  : ON");
+      }
+      else{
+        cmd +=("<br/>Device3  : OFF");
+      }
+      
+      if(device4){
+        cmd +=("<br/>Device4  : ON");
+      }
+      else{
+        cmd +=("<br/>Device4  : OFF");
+      }
+           
+      cmd += "</html>\r\n";
+      server.send(200, "text/html", cmd);
+}
+
+//========================================SETUP=======================================
+void setup(void){
+  pinMode(output1, OUTPUT);
+  pinMode(output2, OUTPUT);
+  pinMode(output3, OUTPUT);
+  pinMode(output4, OUTPUT);
+
+  digitalWrite(output1, LOW); 
+  digitalWrite(output2, LOW);
+  digitalWrite(output3, LOW);
+  digitalWrite(output4, LOW);
+   
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+
+  
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+//============================================RIGHT FUNCTIONS==============================================
+  server.on("/", handleRoot); //wont use HTTP_GET for now
+  
+  server.on("/status1=1", [](){
+    server.send(200, "text/plain", "device1 = ON");
+    digitalWrite(output1, HIGH); 
+    device1 = true;
+  });
+
+  server.on("/status1=0", [](){
+    server.send(200, "text/plain", "device1 = OFF");
+    digitalWrite(output1, LOW); 
+    device1 = false;
+  });
+
+  server.on("/status2=1", [](){
+    server.send(200, "text/plain", "device2 = ON");
+    digitalWrite(output2, HIGH); 
+    device2 = true;
+  });
+
+  server.on("/status2=0", [](){
+    server.send(200, "text/plain", "device2 = OFF");
+    digitalWrite(output2, LOW); 
+    device2 = false;
+  });
+
+  server.on("/status3=1", [](){
+    server.send(200, "text/plain", "device3 = ON");
+    digitalWrite(output3, HIGH); 
+    device3 = true;
+  });
+
+  server.on("/status3=0", [](){
+    server.send(200, "text/plain", "device3 = OFF");
+    digitalWrite(output3, LOW); 
+    device3 = false;
+  });
+
+  server.on("/status4=1", [](){
+    server.send(200, "text/plain", "device4 = ON");
+    digitalWrite(output4, HIGH); 
+    device4 = true;
+  });
+
+  server.on("/status4=0", [](){
+    server.send(200, "text/plain", "device4 = OFF");
+    digitalWrite(output4, LOW); 
+    device4 = false;
+  });
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+//======================================================LOOP================================
+void loop(void){
+  server.handleClient();
+}
---
+++ examples/2.0_Turning_on_off_two_leds_using_browser_wifi_.ino/2.0_Turning_on_off_two_leds_using_browser_wifi_.ino.ino
@@ -0,0 +1,107 @@
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h> 
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+
+ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+
+ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
+
+const int led = 12;
+const int led1= 14;
+
+bool led_=false;
+bool led1_=false;
+
+
+void handleRoot();              // function prototypes for HTTP handlers
+void handleLED();
+void handleLED1();
+void handleNotFound();
+
+String cmd; //to store the html code before we send it
+//========================================SETUP=======================================
+void setup(void){
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+
+  pinMode(led, OUTPUT);
+   pinMode(led1, OUTPUT);
+
+  wifiMulti.addAP("00000", "00000");   // add Wi-Fi networks you want to connect to
+  wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
+  wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
+
+  Serial.println("Connecting ...");
+  int i = 0;
+  while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect: scan for Wi-Fi networks, and connect to the strongest of the networks above
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println('\n');
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());              // Tell us what network we're connected to
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
+
+  if (MDNS.begin("esp8266")) {              // Start the mDNS responder for esp8266.local
+    Serial.println("mDNS responder started");
+  } else {
+    Serial.println("Error setting up MDNS responder!");
+  }
+//======================================RIGHT FUNCTIONS======================================================================
+  server.on("/", HTTP_GET, handleRoot);   // Call the 'handleRoot' function when a client requests URI "/"
+  server.on("/LED", HTTP_POST, handleLED);// Call the 'handleLED' function when a POST request is made to URI "/LED"
+  server.on("/LED1", HTTP_POST, handleLED1);// Call the 'handleLED' function when a POST request is made to URI "/LED"
+  server.onNotFound(handleNotFound);      // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
+
+  server.begin();                         // Actually start the server
+  Serial.println("HTTP server started");
+}
+//==================================================================LOOP======================================================
+void loop(void){
+  server.handleClient();                    // Listen for HTTP requests from clients
+}
+//===================================FUNCTIONS===============================================
+void handleRoot() { // When URI / is requested, send a web page with a button to toggle the LED
+  cmd="";
+ 
+ cmd+= "<!DOCTYPE HTML>\r\n <html>\r\n <header>\r\n <title>ESP8266 Webserver</title>\r\n <h1>\"ESP8266 Web Server Control\"</h1>\r\n </header>\r\n <p>\r\n <form action=\"/LED\" method=\"POST\">\r\n <input type=\"submit\" value=\"Toggle LED\">\r\n </form>\r\n";
+  if(led_){
+    cmd += ("Led: ON \r\n </p>");
+  }
+  else{
+    cmd +=("Led: OFF\r\n </p>");
+  }
+  
+   cmd+="<p>\r\n <form action=\"/LED1\" method=\"POST\">\r\n <input type=\"submit\" value=\"Toggle LED1\">\r\n </form>\r\n ";
+    if(led1_){
+    cmd += ("Led1: ON \r\n </p>");
+  }
+  else{
+    cmd +=("Led1: OFF\r\n </p>");
+  }
+
+  cmd+= "</html>\r\n";
+  
+  server.send(200, "text/html", cmd );
+}
+
+void handleLED() {                          // If a POST request is made to URI /LED
+  digitalWrite(led,!digitalRead(led));      // Change the state of the LED
+  led_=!led_;
+  server.sendHeader("Location","/");        // Add a header to respond with a new location for the browser to go to the home page again
+  server.send(303);                         // Send it back to the browser with an HTTP status 303 (See Other) to redirect
+}
+void handleLED1() {                          // If a POST request is made to URI /LED
+  digitalWrite(led1,!digitalRead(led1));      // Change the state of the LED
+    led1_=!led1_;  
+  server.sendHeader("Location","/");        // Add a header to respond with a new location for the browser to go to the home page again
+  server.send(303);                         // Send it back to the browser with an HTTP status 303 (See Other) to redirect
+}
+
+void handleNotFound(){
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
+
